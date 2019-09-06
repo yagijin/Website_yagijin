e33877888dbad5b38ee7fd59f0c27ae06c4e6fef
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-09-06 17:40:10 +0900

    Updates

---
+++ precache-manifest.5d29d42cfc8e2b543dcb0c22fc091d59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9823924fc08d97c641b86163f6e9a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18a569c47cfaa6cc70a",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "666a5fbf0928beedef68",
+    "url": "/static/js/2.8e9aff88.chunk.js"
+  },
+  {
+    "revision": "d18a569c47cfaa6cc70a",
+    "url": "/static/js/main.c7b10286.chunk.js"
+  },
+  {
+    "revision": "130cb58d60e279bf5b1b",
+    "url": "/static/js/runtime~main.5027abb6.js"
+  }
+]);
