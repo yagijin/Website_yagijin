d4d1dfcfcabf425abef0449e4cbd7a0b35977a48
yagijin <mukailab@mukailabnoMacBook-Pro.local>
2020-01-05 18:31:16 +0900

    Updates

---
+++ precache-manifest.349efdeb709cd7b746aacf61b6ce70fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c342a213315896c21c533c0c6af0e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4519b5118bb424142ab9",
+    "url": "/static/css/main.269a8e55.chunk.css"
+  },
+  {
+    "revision": "eb3c443f26488acc0662",
+    "url": "/static/js/2.6f610379.chunk.js"
+  },
+  {
+    "revision": "4519b5118bb424142ab9",
+    "url": "/static/js/main.19f2560b.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1fdf9505a10ee2889743d648a45a3e2.js"
+  "/precache-manifest.349efdeb709cd7b746aacf61b6ce70fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
