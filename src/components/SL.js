2f1e9cdd6f4b915e207a9abf4456387a29b1f7cb
yagijin <mukailab@mukailabnoMacBook-Pro.local>
2020-01-09 11:12:10 +0900

    FIX:SLコマンドの修正

--- src/components/SL.js
+++ src/components/SL.js
@@ -4,6 +4,6 @@ import SLPic from "../assets/SL.png";
 export default function SL(props) {
     const position = props.do - 40;
     return (
-        props.do?<img src={SLPic} alt="sl-pic" style={{position: "absolute",right: position+"%",height: "50%"}}></img>:<></>
+        props.do?<img src={SLPic} alt="sl-pic" style={{position: "absolute",right: position+"%",height: "50%",zIndex: 2}}></img>:<></>
     );
 }
