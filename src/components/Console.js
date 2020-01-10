50c0fdc822a542563dd0704c2db534d8eda886ee
yagijin <mukailab@mukailabnoMacBook-Pro.local>
2020-01-10 19:04:40 +0900

    FIX:コマンドを間違えた際のサジェスチョンの昨日の追加

--- src/components/Console.js
+++ src/components/Console.js
@@ -22,16 +22,77 @@ export default function Console(){
         return (notPermittedCommands.find(element => element === command));//true or falseを返す
     }
 
+    function consoleClicked() {
+        document.getElementById("inputCommand").focus();
+    }
+
+    function levenshteinDistance(str1, str2) { 
+    
+        const d = []; 
+        for(let i=0;i<=str1.length;i++){ 
+            d[i] = []; 
+            d[i][0] = i; 
+        } 
+        for(let i=0;i<=str2.length;i++){ 
+            d[0][i] = i; 
+        } 
+    
+        let cost = 0; 
+        for(let i=1;i<=str1.length;i++){ 
+            for(let j=1;j<=str2.length;j++){ 
+                cost = (str1[i - 1] === str2[j - 1] ? 0 : 1); 
+                d[i][j] = Math.min( d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost ); 
+            }
+        }
+        //console.log(d[str1.length][str2.length] / Math.max(str1.length,str2.length));
+        return d[str1.length][str2.length] / Math.max(str1.length,str2.length);
+    }
+
     function keyPress(e) {
         let judgeCommand = false;
-        
+        let commandsWithArgs = input.split([" "]);
+
         if(e.key === "ArrowUp" || e.key === "ArrowDown"){
-        if(history.length>2){
-            setInput(history.slice(2));
-        }
+            if(history.length>2){
+                setInput(history.slice(2));
+            }
+        }else if(e.key === "Tab"){
+            //タブのデフォルトの挙動（フォーカスの移動）を止める
+            e.preventDefault();
+
+            //tabが押されたらコマンドをサジェストする
+            for(let i=0;i<content.length;i++){
+                if(content[i].command.indexOf(input)===0){
+                    setInput(content[i].command);
+                    break;
+                }
+            }
+
+            /*
+            let originalCommands = input.split([" "]);
+
+            if(originalCommands[0]!==undefined){
+
+            }else if(originalCommands.length===1){
+                //tabが押されたらコマンドをサジェストする
+                for(let i=0;i<content.length;i++){
+                    if(content[i].command.indexOf(input)===0){
+                        setInput(content[i].command);
+                        break;
+                    }
+                }
+            }else if(originalCommands.length===2){
+                //tabが押されたら引数をサジェストする
+                for(let i=0;i<content.length;i++){
+                    if(originalCommands[0] === content[i].command){
+                        
+                    }
+                }
+            }
+            */
+            console.log("tab");
         }else if(e.key === "Enter"){
             for(let i=0;i<content.length;i++){
-                let commandsWithArgs = input.split([" "]);
                 if(commandsWithArgs[0]===""){
                     judgeCommand = true;
                     break;
@@ -91,16 +152,28 @@ export default function Console(){
                 }
             }
             if(!judgeCommand){
-                setHistory("$ " + input);
-                setCommands(["command not found."]);
+                /*
+                コマンドとの編集距離を求めて，類似度の高いコマンドをサジェストする機能
+                */
+                let min = levenshteinDistance(commandsWithArgs[0],content[0].command);
+                let min_index = 0;
+                for(let i=1;i<content.length;i++){
+                    let distance = levenshteinDistance(commandsWithArgs[0],content[i].command);
+                    if(min>distance){
+                        min_index = i;
+                        min = distance;
+                    }
+                }
+                if(min<0.7){
+                    setHistory("$ " + input);
+                    setCommands(["you'd like to use <"+content[min_index].command+"> ?"]);
+                }else{
+                    setHistory("$ " + input);
+                    setCommands(["command not found."]);
+                }
             }
             setInput("");
         };
-    }
-
-    
-    function consoleClicked() {
-        document.getElementById("inputCommand").focus();
     }
 
     return(
@@ -121,7 +194,7 @@ export default function Console(){
                     <div className="cmd-rootpath">
                     $
                     </div>
-                    <input type="text" id="inputCommand" value={input} autoFocus onChange={(e) => setInput(e.target.value)} className="cmd-command" onKeyDown={(e) => keyPress(e)} >
+                    <input type="text" id="inputCommand" value={input} autoComplete="off" autoFocus onChange={(e) => setInput(e.target.value)} className="cmd-command" onKeyDown={(e) => keyPress(e)} >
                     </input>
                 </div>
             </div>
